9488ec8373a7d428cdeeaabb09d4314ae85cb890
Florencia Vela <flor.p.vela@gmail.com>
2023-06-01 14:10:23 -0300

    installing openvino, cv2 and GStreamer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,12 @@
 
 ################### install openvino ###################
 
-# Step 1: Create a virtual environment for the notebooks
-# Step 5: Create a Conda environment for GStreamer
-echo "Creating Conda environment for GStreamer"
-conda create -n test
-conda activate test
+echo "Creating virtual environment for notebooks"
+python3 -m venv .venv
 
-# Step 3: Install OpenVINO and dependencies
+echo "Activating virtual environment"
+source .venv/bin/activate
+
 echo "Installing OpenVINO and dependencies"
 pip install --upgrade pip
 pip install --no-deps openvino openvino-dev nncf
@@ -17,31 +16,22 @@ pip install -r requirements.txt
 
 ################### install gstreamer ###################
 
-# Step 4: Install GStreamer and dependencies
 echo "Installing GStreamer and dependencies"
 sudo apt update
+sudo apt upgrade
 sudo apt install build-essential
 sudo apt-get install manpages-dev
 sudo apt-get install gstreamer1.0*
 sudo apt install ubuntu-restricted-extras
 sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
-# Step 5: Install additional dependencies for GStreamer
-echo "Installing additional dependencies for GStreamer"
-pip install numpy
-
-# Step 7: Clone and build OpenCV with GStreamer support
 echo "Cloning and building OpenCV with GStreamer support"
+cd ~
 git clone https://github.com/opencv/opencv.git
-
 cd opencv/
-
 git checkout 4.1.0
-
 mkdir build
-
 cd build
-
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D INSTALL_PYTHON_EXAMPLES=ON \
 -D INSTALL_C_EXAMPLES=OFF \
@@ -53,13 +43,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
 -D WITH_GSTREAMER=ON \
 -D BUILD_EXAMPLES=ON ..
-
 sudo make -j$(nproc)
-
 sudo make install
-
 sudo ldconfig
 
-# Step 8: Install GCC with a specific version
-# echo "Installing GCC with a specific version"
 # conda install -c conda-forge gcc=12.1.0
