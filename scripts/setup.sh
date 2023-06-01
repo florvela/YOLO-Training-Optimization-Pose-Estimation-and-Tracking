3e67f5e8ee45ad5c7e8c1a5315632bd9bb061806
Florencia Vela <flor.p.vela@gmail.com>
2023-06-01 14:40:27 -0300

    installing openvino, cv2 and GStreamer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,30 +1,30 @@
-echo "Creating virtual environment for notebooks"
+echo "Creating virtual environment for notebooks" &&
 # python3 -m venv .venv
 # echo "Activating virtual environment"
 # source .venv/bin/activate
 
-&& conda create -n testing python=3
-&& conda activate testing
+conda create -n testing python=3 &&
+conda activate testing &&
 
 ################### install gstreamer ###################
 
-&& echo "Installing GStreamer and dependencies"
-&& sudo apt update
-&& sudo apt upgrade
-&& sudo apt install build-essential
-&& sudo apt-get install manpages-dev
-&& sudo apt-get install gstreamer1.0*
-&& sudo apt install ubuntu-restricted-extras
-&& sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+echo "Installing GStreamer and dependencies" &&
+sudo apt update &&
+sudo apt upgrade &&
+sudo apt install build-essential &&
+sudo apt-get install manpages-dev &&
+sudo apt-get install gstreamer1.0* &&
+sudo apt install ubuntu-restricted-extras &&
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev &&
 
-&& echo "Cloning and building OpenCV with GStreamer support"
-&& cd ~
-&& git clone https://github.com/opencv/opencv.git
-&& cd opencv/
-&& git checkout 4.1.0
-&& mkdir build
-&& cd build
-&& cmake -D CMAKE_BUILD_TYPE=RELEASE \
+echo "Cloning and building OpenCV with GStreamer support" &&
+cd ~ &&
+git clone https://github.com/opencv/opencv.git &&
+cd opencv/ &&
+git checkout 4.1.0 &&
+mkdir build &&
+cd build &&
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D INSTALL_PYTHON_EXAMPLES=ON \
 -D INSTALL_C_EXAMPLES=OFF \
 -D PYTHON_EXECUTABLE=$(which python3) \
@@ -34,16 +34,16 @@ echo "Creating virtual environment for notebooks"
 -D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
 -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
 -D WITH_GSTREAMER=ON \
--D BUILD_EXAMPLES=ON ..
-&& sudo make -j$(nproc)
-&& sudo make install
-&& sudo ldconfig
-&& conda install -c conda-forge gcc=12.1.0
+-D BUILD_EXAMPLES=ON .. &&
+sudo make -j$(nproc) &&
+sudo make install &&
+sudo ldconfig &&
+conda install -c conda-forge gcc=12.1.0 &&
 
 
 ################### install openvino ###################
 
-&& echo "Installing OpenVINO and dependencies"
-&& pip install --upgrade pip
-&& pip install --no-deps openvino openvino-dev nncf
-&& pip install -r requirements.txt
+echo "Installing OpenVINO and dependencies" &&
+pip install --upgrade pip &&
+pip install --no-deps openvino openvino-dev nncf &&
+pip install -r requirements.txt
