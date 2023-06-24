9ef5ce2ca097d199e314dbdd345881e62bcd248d
Flor Vela <flor.p.vela@gmail.com>
2023-06-24 11:20:06 -0300

    optimizing with openvino

--- scripts/install_openvino.sh
+++ scripts/install_openvino.sh
@@ -5,14 +5,14 @@
 ######## install openvino ########
 
 # Step 1: Create a virtual environment for the notebooks
-echo "Creating virtual environment for notebooks"
-&& python3 -m venv .venv
+echo "Creating virtual environment for notebooks" &&
+python3 -m venv .openvino_venv &&
 
 # Step 2: Activate the virtual environment
-&& echo "Activating virtual environment"
-&& source .venv/bin/activate
+echo "Activating virtual environment" &&
+source .openvino_venv/bin/activate &&
 
 # sudo apt install python3-pip
-&& pip install --upgrade pip
-&& pip install --no-deps openvino openvino-dev nncf
-&& pip install -r requirements.txt
+pip install --upgrade pip &&
+pip install --no-deps openvino openvino-dev nncf &&
+pip install -r requirements.txt
