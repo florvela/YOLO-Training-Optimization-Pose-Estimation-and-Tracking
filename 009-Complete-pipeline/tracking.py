f0ed55d16445c5b665bacdd7161f8638774b69e8
Florencia Vela <flor.p.vela@gmail.com>
2023-08-06 21:40:34 -0300

    tracking with yolov8 pose and bytetrack

--- 009-Complete-pipeline/tracking.py
+++ 009-Complete-pipeline/tracking.py
@@ -13,31 +13,40 @@ from supervision.notebook.utils import show_frame_in_notebook
 from supervision.tools.detections import Detections, BoxAnnotator
 import pdb
 import torch
-import math
-import cv2
-
+import argparse
 import os
+import sys
 HOME = os.getcwd()
 print(HOME)
-
-import sys
 sys.path.append(f"{HOME}/ByteTrack")
 from yolox.tracker.byte_tracker import BYTETracker, STrack
 
-# SOURCE_VIDEO_PATH = f"{HOME}/video1.mp4"
-# TARGET_VIDEO_PATH = f"{HOME}/video1-result.mp4"
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Your script description")
+    parser.add_argument("--output", default=".", help="Output directory path")
+    parser.add_argument("--video", default="../videos_for_testing/example.mp4", help="Input video filename")
+    parser.add_argument("--method", default="nearby-hand", help="Either nearby-hand or touching_boxes", choices=["nearby-hand", "touching-boxes"])
 
-# Check if a filename is provided as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: python3 tracking.py <input_video_filename>")
-    sys.exit(1)
+    args = parser.parse_args()
+    return args
 
-# Get the video filename from the command-line arguments
-video_filename = sys.argv[1]
+
+args = parse_arguments()
+
+# Access the output directory path from the command-line arguments
+output_directory = args.output
+video_path = args.video
+video_filename = os.path.basename(video_path)
+method = args.method
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 # Generate the source and target video paths using the provided filename
-SOURCE_VIDEO_PATH = os.path.join(HOME, video_filename)
-TARGET_VIDEO_PATH = os.path.join(HOME, f"result-{video_filename}")
+SOURCE_VIDEO_PATH = os.path.join(HOME, video_path)
+TARGET_VIDEO_PATH = os.path.join(output_directory, f"result-{method}-{video_filename}")
+# SOURCE_VIDEO_PATH = f"{HOME}/video1.mp4"
+# TARGET_VIDEO_PATH = f"{HOME}/video1-result.mp4"
 
 # Now you can use SOURCE_VIDEO_PATH and TARGET_VIDEO_PATH in your script
 print(f"Source video path: {SOURCE_VIDEO_PATH}")
@@ -92,8 +101,8 @@ def match_detections_with_tracks(
     return tracker_ids
 
 def euclidean_distance(point1, point2):
-    # return np.sqrt(np.sum((point1 - point2) ** 2))
-    return math.sqrt( ((point1[0]-point2[0])**2)+((point1[1]-point2[1])**2) )
+    return np.sqrt(np.sum((point1 - point2) ** 2))
+    # return math.sqrt( ((point1[0]-point2[0])**2)+((point1[1]-point2[1])**2) )
 
 def find_closest_keypoint(keypoints, bounding_box):
     # Convert bounding box to [x, y] format
@@ -117,26 +126,6 @@ def find_closest_keypoint(keypoints, bounding_box):
 
     return closest_index
 
-
-gun_model = YOLO(os.path.join(HOME,'weights/guns.pt'))
-model = YOLO(os.path.join(HOME,'weights/yolov8n-pose.pt'))
-gun_model.fuse()
-model.fuse()
-
-# class_ids of interest - car, motorcycle, bus and truck
-POSE_INTEREST_IDS = [0] # 0 para personas
-
-
-# create BYTETracker instance
-byte_tracker = BYTETracker(BYTETrackerArgs())
-# create VideoInfo instance
-video_info = VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
-# create frame generator
-generator = get_video_frames_generator(SOURCE_VIDEO_PATH)
-# create instance of BoxAnnotator and LineCounterAnnotator
-box_annotator = BoxAnnotator(color=ColorPalette(), thickness=2, text_thickness=1, text_scale=0.5)
-
-
 def are_boxes_touching(box1, box2):
     x_min1, y_min1, x_max1, y_max1 = box1
     x_min2, y_min2, x_max2, y_max2 = box2
@@ -153,98 +142,175 @@ def are_boxes_touching(box1, box2):
     return True
 
 
+gun_model = YOLO(os.path.join(HOME,'weights/guns.pt'))
+gun_model.fuse()
+
+model = YOLO(os.path.join(HOME,'weights/yolov8n-pose.pt'))
+model.fuse()
+
+# class_ids of interest - car, motorcycle, bus and truck
+POSE_INTEREST_IDS = [0] # 0 para personas
+
+# create BYTETracker instance
+byte_tracker = BYTETracker(BYTETrackerArgs())
+# create VideoInfo instance
+video_info = VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+# create frame generator
+generator = get_video_frames_generator(SOURCE_VIDEO_PATH)
+# create instance of BoxAnnotator and LineCounterAnnotator
+box_annotator = BoxAnnotator(color=ColorPalette(), thickness=2, text_thickness=1, text_scale=0.5)
+
+
 follow_det = -1
-
 flag = False
-
 track_true = set()
 
 
 # open target video file
 with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
     # loop over video frames
-    counter = 0
-    for frame in tqdm(generator, total=video_info.total_frames):
-        gun_results = gun_model(frame, conf=0.5)
+    if method == "nearby-hand":
+        for frame in tqdm(generator, total=video_info.total_frames):
+            gun_results = gun_model(frame, conf=0.5)
 
-        # model prediction on single frame and conversion to supervision Detections
-        results = model(frame, conf=0.7)
-        frame = results[0].plot(boxes=False)
+            # model prediction on single frame and conversion to supervision Detections
+            results = model(frame, conf=0.7)
+            frame = results[0].plot(boxes=False)
 
-        ################ Cambiar esto para ver si lo agarracon la mano y no solo si el arma esta cerca de la persona
-        touching_indexes = []
-        not_touching_indexes = []
 
-        if len(gun_results[0]) > 0:
-            for idx2, box2 in enumerate(results[0].boxes.xyxy):
-                for box1 in gun_results[0].boxes.xyxy:
-                    if are_boxes_touching(box1, box2):
-                        touching_indexes.append(idx2)
-                        break  # No need to check other boxes in boxes1 for this box2
-                if idx2 not in touching_indexes:
-                   not_touching_indexes.append(idx2)
-        else:
+            detections = Detections(
+                xyxy=results[0].boxes.xyxy.cpu().numpy(),
+                confidence=results[0].boxes.conf.cpu().numpy(),
+                class_id=results[0].boxes.cls.cpu().numpy().astype(int)
+            )
+            
+            # filtering out detections with unwanted classes
+            mask = np.array([class_id in POSE_INTEREST_IDS for class_id in detections.class_id], dtype=bool)
+            detections.filter(mask=mask, inplace=True)
+
+            # tracking detections
+            tracks = byte_tracker.update(
+                output_results=detections2boxes(detections=detections),
+                img_info=frame.shape,
+                img_size=frame.shape
+            )
+            tracker_id = match_detections_with_tracks(detections=detections, tracks=tracks)
+            detections.tracker_id = np.array(tracker_id)
+
+            keep_track = list()
+            if len(gun_results[0]) and len(results[0]):
+                # coordenadas x,y de las munecas son 10 y 9
+                result_hand_keypoints = [sub_array[[9,10]] for sub_array in results[0].keypoints.xy]
+
+                for bounding_box in gun_results[0].boxes.xyxy:
+                    keep_track.append(find_closest_keypoint(result_hand_keypoints, bounding_box))
+
+            try:
+                if len(detections.tracker_id):
+                    for tracker_id in detections.tracker_id[keep_track]:
+                        track_true.add(tracker_id)
+            except:
+                pdb.set_trace()
+
+            mask = np.array([tracker_id is not None and tracker_id in track_true for tracker_id in detections.tracker_id], dtype=bool)
+            detections.filter(mask=mask, inplace=True)
+
+            labels = [
+                f"#{tracker_id} {confidence:0.2f}"
+                for _, confidence, class_id, tracker_id
+                in detections
+            ]
+            # annotate and display frame
+            frame = box_annotator.annotate(frame=frame, detections=detections, labels=labels)
+
+            ### Anotar las guns!!!! 
+            detections2 = Detections(
+                xyxy=gun_results[0].boxes.xyxy.cpu().numpy(),
+                confidence=gun_results[0].boxes.conf.cpu().numpy(),
+                class_id=gun_results[0].boxes.cls.cpu().numpy().astype(int)
+            )
+            labels = [
+                f"{gun_model.model.names[class_id]} {confidence:0.2f}"
+                for _, confidence, class_id, _
+                in detections2
+            ]
+            frame = box_annotator.annotate(frame=frame, detections=detections2, labels=labels)
+
+            sink.write_frame(frame)
+    else:
+        for frame in tqdm(generator, total=video_info.total_frames):
+            gun_results = gun_model(frame, conf=0.5)
+
+            # model prediction on single frame and conversion to supervision Detections
+            results = model(frame, conf=0.7)
+            frame = results[0].plot(boxes=False)
+
+            ################ Cambiar esto para ver si lo agarracon la mano y no solo si el arma esta cerca de la persona
             touching_indexes = []
             not_touching_indexes = []
 
-
-        detections = Detections(
-            xyxy=results[0].boxes.xyxy.cpu().numpy(),
-            confidence=results[0].boxes.conf.cpu().numpy(),
-            class_id=results[0].boxes.cls.cpu().numpy().astype(int)
-        )
-        
-        # filtering out detections with unwanted classes
-        mask = np.array([class_id in POSE_INTEREST_IDS for class_id in detections.class_id], dtype=bool)
-        detections.filter(mask=mask, inplace=True)
-
-        # tracking detections
-        tracks = byte_tracker.update(
-            output_results=detections2boxes(detections=detections),
-            img_info=frame.shape,
-            img_size=frame.shape
-        )
-        tracker_id = match_detections_with_tracks(detections=detections, tracks=tracks)
-        detections.tracker_id = np.array(tracker_id)
-
-        keep_track = list()
-        if len(gun_results[0]) and len(results[0]):
-            # coordenadas x,y de las munecas son 10 y 9
-            result_hand_keypoints = [sub_array[[9,10]] for sub_array in results[0].keypoints.xy]
-
-            for bounding_box in gun_results[0].boxes.xyxy:
-                keep_track.append(find_closest_keypoint(result_hand_keypoints, bounding_box))
-
-        try:
-            if len(detections.tracker_id):
-                for tracker_id in detections.tracker_id[keep_track]:
-                    track_true.add(tracker_id)
-        except:
-            pdb.set_trace()
-
-        mask = np.array([tracker_id is not None and tracker_id in track_true for tracker_id in detections.tracker_id], dtype=bool)
-        detections.filter(mask=mask, inplace=True)
-
-        labels = [
-            f"#{tracker_id} {confidence:0.2f}"
-            for _, confidence, class_id, tracker_id
-            in detections
-        ]
-        # annotate and display frame
-        frame = box_annotator.annotate(frame=frame, detections=detections, labels=labels)
+            if len(gun_results[0]) > 0:
+                for idx2, box2 in enumerate(results[0].boxes.xyxy):
+                    for box1 in gun_results[0].boxes.xyxy:
+                        if are_boxes_touching(box1, box2):
+                            touching_indexes.append(idx2)
+                            break  # No need to check other boxes in boxes1 for this box2
+                    if idx2 not in touching_indexes:
+                       not_touching_indexes.append(idx2)
+            else:
+                touching_indexes = []
+                not_touching_indexes = []
 
 
-        ### Anotar las guns!!!! 
-        detections2 = Detections(
-            xyxy=gun_results[0].boxes.xyxy.cpu().numpy(),
-            confidence=gun_results[0].boxes.conf.cpu().numpy(),
-            class_id=gun_results[0].boxes.cls.cpu().numpy().astype(int)
-        )
-        labels = [
-            f"{gun_model.model.names[class_id]} {confidence:0.2f}"
-            for _, confidence, class_id, _
-            in detections2
-        ]
-        frame = box_annotator.annotate(frame=frame, detections=detections2, labels=labels)
+            detections = Detections(
+                xyxy=results[0].boxes.xyxy.cpu().numpy(),
+                confidence=results[0].boxes.conf.cpu().numpy(),
+                class_id=results[0].boxes.cls.cpu().numpy().astype(int)
+            )
+            
+            # filtering out detections with unwanted classes
+            mask = np.array([class_id in POSE_INTEREST_IDS for class_id in detections.class_id], dtype=bool)
+            detections.filter(mask=mask, inplace=True)
 
-        sink.write_frame(frame)
+            # tracking detections
+            tracks = byte_tracker.update(
+                output_results=detections2boxes(detections=detections),
+                img_info=frame.shape,
+                img_size=frame.shape
+            )
+            tracker_id = match_detections_with_tracks(detections=detections, tracks=tracks)
+            detections.tracker_id = np.array(tracker_id)
+
+            try:
+                if len(detections.tracker_id):
+                    for tracker_id in detections.tracker_id[touching_indexes]:
+                        track_true.add(tracker_id)
+            except:
+                pdb.set_trace()
+
+            mask = np.array([tracker_id is not None and tracker_id in track_true for tracker_id in detections.tracker_id], dtype=bool)
+            detections.filter(mask=mask, inplace=True)
+
+            labels = [
+                f"#{tracker_id} {confidence:0.2f}"
+                for _, confidence, class_id, tracker_id
+                in detections
+            ]
+            # annotate and display frame
+            frame = box_annotator.annotate(frame=frame, detections=detections, labels=labels)
+
+
+            ### Anotar las guns!!!! 
+            detections2 = Detections(
+                xyxy=gun_results[0].boxes.xyxy.cpu().numpy(),
+                confidence=gun_results[0].boxes.conf.cpu().numpy(),
+                class_id=gun_results[0].boxes.cls.cpu().numpy().astype(int)
+            )
+            labels = [
+                f"{gun_model.model.names[class_id]} {confidence:0.2f}"
+                for _, confidence, class_id, _
+                in detections2
+            ]
+            frame = box_annotator.annotate(frame=frame, detections=detections2, labels=labels)
+
+            sink.write_frame(frame)
