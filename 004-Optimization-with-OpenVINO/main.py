98c44efad81c2c55c4bc2aebeca268297f0a1b36
Flor Vela <flor.p.vela@gmail.com>
2023-08-31 10:09:09 -0300

    optimize with openVINO

--- 004-Optimization-with-OpenVINO/main.py
+++ 004-Optimization-with-OpenVINO/main.py
@@ -16,6 +16,7 @@ from typing import Any, Dict, Tuple
 import numpy as np
 import openvino.runtime as ov
 import torch
+import os
 from tqdm import tqdm
 from ultralytics import YOLO
 from ultralytics.yolo.cfg import get_cfg
@@ -84,6 +85,7 @@ def prepare_validation(model: YOLO, args: Any, images_path: str, yaml_path: str)
     # dataset = YOLODataset(images_path, data=data)
     # data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
+    # dataset = validator.data["val"]
     dataset = validator.data["val"]
     data_loader = validator.get_dataloader(dataset, 1)
 
@@ -153,19 +155,17 @@ def quantize(model: ov.Model, data_loader: torch.utils.data.DataLoader, validato
     return quantized_model
 
 
-def main():
+def optimize(weights_dir, dataset_dir):
     MODEL_NAME = "best"
-    weights_dir = "/home/flor/Desktop/git_repos/YOLO-OpenVINO-TVM-GStreamer/002-Training-models/train_results/model_yolov8_tf_yolov8s_imgsz_800_epochs_78_batch_16_dataset_v2/weights/"
 
-    images_path = "/home/flor/Desktop/git_repos/YOLO-OpenVINO-TVM-GStreamer/datasets/yolov8_original_no_empty/valid/images"
-    yaml_path = "/home/flor/Desktop/git_repos/YOLO-OpenVINO-TVM-GStreamer/datasets/yolov8_original_no_empty/data.yaml"
+    images_path = os.path.join(dataset_dir, "test/images")
+    yaml_path = os.path.join(dataset_dir, "data.yaml")
 
     # model = YOLO(f"yolov8s.pt")
-    model = YOLO(f"/home/flor/Desktop/git_repos/YOLO-OpenVINO-TVM-GStreamer/002-Training-models/train_results/model_yolov8_tf_yolov8s_imgsz_800_epochs_78_batch_16_dataset_v2/weights/{MODEL_NAME}.pt")
+    model = YOLO(os.path.join(weights_dir, f"{MODEL_NAME}.pt"))
     args = get_cfg(cfg=DEFAULT_CFG)
-    args.data = "/home/flor/Desktop/git_repos/YOLO-OpenVINO-TVM-GStreamer/datasets/yolov8_original_no_empty/data.yaml"
+    args.data = yaml_path
 
-    
     # Prepare validation dataset and helper
     validator, data_loader = prepare_validation(model, args, images_path, yaml_path)
     
@@ -196,7 +196,40 @@ def main():
     print(f"Quantized model performance: {quantized_model_perf} FPS")
 
     return fp_stats["metrics/mAP50-95(B)"], q_stats["metrics/mAP50-95(B)"], fp_model_perf, quantized_model_perf
-    
+
+
+def main():
+    base_dir = os.path.dirname(os.getcwd())
+    models_dir = os.path.join(base_dir, "002-Training-models/train_results/chosen_models")
+    dataset_dir = os.path.join(base_dir, "datasets/yolov8_rc")
+
+    for foldername in os.listdir(models_dir):
+        variables = foldername.split('_')
+        model = variables[1]
+        tf = variables[3]
+        imgsz = variables[5]
+        epochs = variables[7]
+        batch = variables[9]
+        dataset = variables[11]
+        loss = variables[13]
+        lr = variables[15]
+
+        print(foldername)
+        print("model", model)
+        print("tf", tf)
+        print("imgsz", imgsz)
+        print("epochs", epochs)
+        print("batch", batch)
+        print("dataset", dataset)
+        print("optimizer", loss)
+        print("lr", lr)
+
+        weights_dir = os.path.join(models_dir, foldername, "weights")
+        print(weights_dir)
+        print(dataset_dir)
+
+        optimize(weights_dir, dataset_dir)
+
 
 
 if __name__ == "__main__":
